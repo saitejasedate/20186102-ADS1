ef76b33f7ca05aaee7f1f2da2a2674adb2d24072
saitejasedate <saitejavelagandula@msitprogram.net>
2018-09-27 03:09:22 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

---
+++ assignments/m3/Sorted_Array/Solution.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Solution{
+	public void mergeArrays(final int[] arr1,final int[] arr2){
+		int i = 0;
+		int j = 0;
+		int k = 0;
+		int[] sortedarr = new int[arr1.length + arr2.length];
+		while (i < arr1.length && j < arr2.length){
+			if (arr1[i] < arr2[j]) {
+				sortedarr[k++] = arr1[i];
+				i++;
+			} else {
+				sortedarr[k++] = arr2[j];
+				j++;
+			}
+		}
+			if (i == arr1.length){
+				while (j < arr2.length){
+					sortedarr[k++] = arr2[j];
+					j++;
+				}
+			}
+			else {
+					while(i < arr1.length) {
+					sortedarr[k++] = arr1[i];
+					i++;
+
+				}
+			}
+		int x;
+		for (x = 0; x < sortedarr.length-1; x++) {
+			System.out.print(sortedarr[x]+ ",");
+		}
+		System.out.println(sortedarr[x]);
+	}
+	/**.
+	 * { function_description }
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int sizeOfarr1 = sc.nextInt();
+		int sizeOfarr2 = sc.nextInt();
+		int[] arr1 = new int[sizeOfarr1];
+		int[] arr2 = new int[sizeOfarr2];
+		String[] elements;
+		if (sizeOfarr1 != 0){
+		 elements = sc.nextLine().split(",");
+		for (int i = 0; i < elements.length; i++) {
+			arr1[i] = Integer.parseInt(elements[i]);
+		}
+		} else{
+			sc.nextLine();
+	}
+	    if (sizeOfarr2 != 0) {
+		elements = sc.nextLine().split(",");
+		for (int i = 0; i < elements.length; i++) {
+			arr2[i] = Integer.parseInt(elements[i]);
+		}
+	}
+		new Solution().mergeArrays(arr1, arr2);
+	}
+}
