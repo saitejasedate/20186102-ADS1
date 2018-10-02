7c3d03bd58974f5e963030fc8c6bbd13b147e723
saitejasedate <saitejavelagandula@msitprogram.net>
2018-10-03 01:36:37 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- assignments/m8/8.1 Team Ranking With Selection/Selection.java
+++ assignments/m8/8.1 Team Ranking With Selection/Selection.java
@@ -1,28 +1,28 @@
 class Selection {
-	/**
-	 * { function_description }.
-	 *
-	 * @param      arr   The arr
-	 */
-	public static void sort(Comparable[] arr) {
-		int arr_length = arr.length;
-		for (int i = 0; i < arr_length; i++) {
-			int min = i;
-			for (int j = i + 1; j < arr_length; j++) {
-				if (less(arr[j], arr[min])) {
-					min = j;
-				}
-			}
-			exch (arr, i, min);
-		}
-		/**Time complexity of the above method is O(N^2log(N)).**/
-	}
-	private static boolean less(Comparable v, Comparable w) {
-		return (v.compareTo(w) < 0);
-	}
-	private static void exch(Comparable[] a, int i, int j) {
-		Comparable t = a[i];
-		a[i] = a[j];
-		a[j] = t;
-	}
+    /**
+     * { function_description }.
+     *
+     * @param      arr   The arr
+     */
+    public static void sort(Comparable[] arr) {
+        int arr_length = arr.length;
+        for (int i = 0; i < arr_length; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr_length; j++) {
+                if (less(arr[j], arr[min])) {
+                    min = j;
+                }
+            }
+            exch (arr, i, min);
+        }
+        /**Time complexity of the above method is O(N^2log(N)).**/
+    }
+    private static boolean less(Comparable v, Comparable w) {
+        return (v.compareTo(w) < 0);
+    }
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
 }
