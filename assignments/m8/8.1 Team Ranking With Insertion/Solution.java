e1ca9ae4bc4c4af47299e365aa65fe80ec7dcf4b
saitejasedate <saitejavelagandula@msitprogram.net>
2018-10-02 18:14:55 +0530

    "check_style_score -> 1 of 6 passed. style score: 0/1 "

---
+++ assignments/m8/8.1 Team Ranking With Insertion/Solution.java
@@ -0,0 +1,94 @@
+import java.util.*;
+	class Team implements Comparable<Team> {
+	private String country;
+	private int wins;
+	private int loses;
+	private int draws;
+	Team (String country, int wins, int loses, int draws) {
+		this.country = country;
+		this.wins = wins;
+		this.loses = loses;
+		this.draws = draws;
+	}
+	public String getcountry() {
+	return country;
+	}
+
+	public void setcountry(String country) {
+	this.country = country;
+	}
+
+	public int getloses() {
+	return loses;
+	}
+
+	public void setloses(int loses) {
+	this.loses = loses;
+	}
+
+	public int getdraws() {
+	return draws;
+	}
+
+	public void setdraws(int draws) {
+	this.draws = draws;
+	}
+	public String toString() {
+		return this.country;
+	}
+	public int compareTo(Team that) {
+		Team a = this;
+		if (a.wins < that.wins) return -1;
+		if (a.wins > that.wins) return +1;
+		if (a.loses < that.loses) return -1;
+		if (a.loses > that.loses) return +1;
+		if (a.draws < that.draws) return -1;
+		if (a.draws > that.draws) return +1; 
+		return 0;
+	}
+}
+
+class Selection_sort {
+	public static void sort(Comparable[] arr) {
+		int arr_length = arr.length;
+		for (int i = 0; i < arr_length; i++) {
+			int min = i;
+			for (int j = i + 1; j < arr_length; j++) {
+				if (less(arr[j], arr[min])) {
+					min = j;
+				}
+			}
+			exch (arr, i, min);
+		}
+	}
+	private static boolean less(Comparable v, Comparable w) {
+		return (v.compareTo(w) < 0);
+	}
+	private static void exch(Comparable[] a, int i, int j) {
+		Comparable t = a[i];
+		a[i] = a[j];
+		a[j] = t;
+	}
+}
+
+class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuffer sb = new StringBuffer();
+		while (sc.hasNext()) {
+			sb.append(sc.nextLine() + "::");
+		}
+		String[] lines = sb.toString().split("::");
+		int i = 0;
+		Team[] teams = new Team[lines.length];
+		for (String line : lines) {
+			String[] tokens = line.split(",");
+			Team team = new Team(tokens[0], Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2]), 
+			Integer.parseInt(tokens[3]));
+			teams[i++] = team;
+		} 
+		for (Team team : teams) {
+			System.out.println(team);
+		}
+	}	
+}
