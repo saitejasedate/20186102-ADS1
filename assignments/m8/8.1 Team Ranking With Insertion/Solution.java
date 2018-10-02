79c2a568d2e7d3268ad1a58ed7ac778695aa7f75
saitejasedate <saitejavelagandula@msitprogram.net>
2018-10-02 18:28:17 +0530

    "check_style_score -> 1 of 6 passed. style score: 0/1 "

--- assignments/m8/8.1 Team Ranking With Insertion/Solution.java
+++ assignments/m8/8.1 Team Ranking With Insertion/Solution.java
@@ -38,12 +38,12 @@ import java.util.*;
 	}
 	public int compareTo(Team that) {
 		Team a = this;
-		if (a.wins < that.wins) return -1;
-		if (a.wins > that.wins) return +1;
-		if (a.loses < that.loses) return -1;
-		if (a.loses > that.loses) return +1;
-		if (a.draws < that.draws) return -1;
-		if (a.draws > that.draws) return +1; 
+		if (a.wins > that.wins) return -1;
+		if (a.wins < that.wins) return +1;
+		if (a.loses > that.loses) return -1;
+		if (a.loses < that.loses) return +1;
+		if (a.draws > that.draws) return -1;
+		if (a.draws < that.draws) return +1; 
 		return 0;
 	}
 }
