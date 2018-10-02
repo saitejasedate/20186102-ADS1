c71a9acf4c7752edd72e911594484f338628117c
saitejasedate <saitejavelagandula@msitprogram.net>
2018-10-02 19:28:54 +0530

    "check_style_score -> 0 of 6 passed. style score: 0/1 "

--- assignments/m8/8.1 Team Ranking With Insertion/Solution.java
+++ assignments/m8/8.1 Team Ranking With Insertion/Solution.java
@@ -91,8 +91,9 @@ class Solution {
 		} 
 		Selection_sort.sort(teams);
 		for (Team team : teams) {
-			String output = team + ","; 
-			System.out.print(output.replaceAll(",$", ""));
+			String output = team + ",";
+			output.replaceAll(",$", ""); 
+			System.out.print(output);
 		}
 	}	
 }
