87a55468c2c6965dce92349243d01ee74350650a
saitejasedate <saitejavelagandula@msitprogram.net>
2018-10-05 04:26:41 +0530

    "ads1_m8_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m8/8.1 Team Ranking With Selection/Selection.java
+++ assignments/m8/8.1 Team Ranking With Selection/Selection.java
@@ -1,8 +1,13 @@
+/**
+ * Class for selection.
+ */
 class Selection {
     /**
-     * { function_description }.
-     *
-     * @param      arr   The arr
+     * { sort method for Insertion sort. It sorts all the elements in ascending order }
+     * Complexity : N^2.The complexity for less method is 1
+     * and exch method is 1 and inner loop iterates for N times and
+     * outer loop iterates for another N times so overall time complexity is N**2.
+     * @param      a     { comparable array }
      */
     public static void sort(Comparable[] arr) {
         int arr_length = arr.length;
@@ -15,11 +20,25 @@ class Selection {
             }
             exch (arr, i, min);
         }
-        /**Time complexity of the above method is O(N^2log(N)).**/
     }
+    /**
+     * compares v and w.
+     *
+     * @param      v     { index element of a }
+     * @param      w     { index element of a }
+     *complexity is 1 because the only one statement is executed.
+     * @return     { returns true if condition statisfies}
+     */
     private static boolean less(Comparable v, Comparable w) {
         return (v.compareTo(w) < 0);
     }
+    /**
+     * { exchanges the elements in ith index with jth index}
+     *The time complexity is 1. Because the statements in this method are executed only once.
+     * @param      a     { comparable array }
+     * @param      i     { index of a }
+     * @param      j     { index of a }
+     */
     private static void exch(Comparable[] a, int i, int j) {
         Comparable t = a[i];
         a[i] = a[j];
--- assignments/m8/8.1 Team Ranking With Selection/Team.java
+++ assignments/m8/8.1 Team Ranking With Selection/Team.java
@@ -1,3 +1,6 @@
+/**
+ * Class for team.
+ */
 class Team implements Comparable<Team> {
 	/**
 	 * The team class implements the interface
@@ -21,34 +24,85 @@ class Team implements Comparable<Team> {
 		this.loses = loses;
 		this.draws = draws;
 	}
+	/**
+	 * Gets country.
+	 *
+	 * @return     country.
+	 */
 	public String getcountry() {
 		return country;
 	}
-
+	/**
+	 * Sets country.
+	 *
+	 * @param      country  The country.
+	 */
 	public void setcountry(String country) {
 		this.country = country;
 	}
-
+	/**
+	 * Get wins.
+	 *
+	 * @return     wins.
+	 */
+	public int getwins(){
+		return wins;
+	}
+	/**
+	 * Set wins.
+	 *
+	 * @param      wins  The wins.
+	 */
+	public void setwins(int wins){
+	  	this.wins = wins;
+	}
+	/**
+	 * Get loses.
+	 *
+	 * @return     loses.
+	 */
 	public int getloses() {
 		return loses;
 	}
-
+	/**
+	 * Set loses.
+	 *
+	 * @param      loses  The loses.
+	 */
 	public void setloses(int loses) {
 		this.loses = loses;
 	}
-
+	/**
+	 * Get draws.
+	 *
+	 * @return     draws.
+	 */
 	public int getdraws() {
 		return draws;
 	}
-
+	/**
+	 * Set draws.
+	 *
+	 * @param      draws  The draws.
+	 */
 	public void setdraws(int draws) {
 		this.draws = draws;
 	}
-
+	/**
+	 * Returns a string representation of the object.
+	 *
+	 * @return     String representation of the object.
+	 */
 	public String toString() {
 		return this.country;		
 	}
-
+	/**
+	 * comapares two objects in the given condition.
+	 *
+	 * @param      that  The that
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	public int compareTo(Team that) {
 		Team a = this;
 		if (a.wins > that.wins) return -1;
