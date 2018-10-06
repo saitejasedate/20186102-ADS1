5f98693078b1b81be742360a028cbed0858e6026
saitejasedate <saitejavelagandula@msitprogram.net>
2018-10-06 11:19:33 +0530

    "ads1_m12_a1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ assignments/m12/Assignment-1/Solution.java
@@ -0,0 +1,56 @@
+/**
+ * import Scanner class
+ */
+import java.util.Scanner;
+/**
+ * @author : saitejasedate.
+ */
+final class Solution {
+    /**
+     * Solution class for Insertion sort program.
+     *
+     */
+    private Solution() {
+        /**
+         * { item_description }.
+         */
+    }
+    /**
+     * Main function to take inputs.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+    /**main method converts the given input data into
+    array objects and pass as parameters to the sorting class.**/
+        final int three = 3;
+        Scanner sc = new Scanner(System.in);
+        int studentsQualified = sc.nextInt();
+        int numOfvacancies = sc.nextInt();
+        int unreserved_vacancies = sc.nextInt();
+        int BC_vacancies = sc.nextInt();
+        int SC_vacancies = sc.nextInt();
+        int ST_vacancies = sc.nextInt();
+        StringBuffer sb = new StringBuffer();
+        while (sc.hasNext()) {
+            sb.append(sc.nextLine() + "::");
+        }
+        String[] lines = sb.toString().split("::");
+        int i = 0;
+        Student[] students = new Student[lines.length];
+        for (String line : lines) {
+            String[] tokens = line.split(",");
+            Student student = new Student(tokens[0], tokens[1],
+                Integer.parseInt(tokens[2]), Integer.parseInt(tokens[three]),
+                Integer.parseInt(tokens[4]), Integer.parseInt(tokens[5]), 
+                tokens[6]);
+            students[i++] = student;
+        }
+        Insertion.sort(students);
+        String output = "";
+        for (Student student : students) {
+            output += student + ",";
+        }
+        System.out.print(output.substring(0, output.length() - 1));
+    }
+}
