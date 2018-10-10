a75b800f56adec85b0acd03d1ffc244596236265
saitejasedate <saitejavelagandula@msitprogram.net>
2018-10-11 03:59:05 +0530

    "ads1_m14_a1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m14/14.1 Dynamic Median Finding/Solution.java
+++ assignments/m14/14.1 Dynamic Median Finding/Solution.java
@@ -8,47 +8,48 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-public class Solution {
-	private Solution() {
+final public class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * main function to take input and calculate the median.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int numOfvalues = sc.nextInt();
-		MinPQ<Float> minpq = new MinPQ<Float>(numOfvalues);
-		MaxPQ<Float> maxpq = new MaxPQ<Float>(numOfvalues);
-		double median = 0.0; 
-		while (sc.hasNext()) {
-			for (int i = 0; i < numOfvalues; i++) {
-				float value = sc.nextFloat();
-				if(value > median) {
-					minpq.insert(value);
-				} else {
-					maxpq.insert(value);
-				}
-				if (maxpq.size() - minpq.size() > 1) {
-					float value1 = maxpq.delMax();
-					minpq.insert(value1);
-				} else if (minpq.size() - maxpq.size() > 1) {
-					float value1 = minpq.delMin();
-					maxpq.insert(value1);
-				}
-				if (maxpq.size() == minpq.size()) {
-					median = (minpq.min() + maxpq.max()) / 2;
-				}  
-				else if (maxpq.size() > minpq.size()) {
-					median = maxpq.max();
-				} 
-				else {
-					median = minpq.min();
-				}
-				System.out.println(median);
-			}
-		}	
-	}
+    }
+    /**
+     * main function to take input and calculate the median.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numOfvalues = sc.nextInt();
+        MinPQ<Float> minpq = new MinPQ<Float>(numOfvalues);
+        MaxPQ<Float> maxpq = new MaxPQ<Float>(numOfvalues);
+        double median = 0.0;
+        while (sc.hasNext()) {
+            for (int i = 0; i < numOfvalues; i++) {
+                float value = sc.nextFloat();
+                if (value > median) {
+                    minpq.insert(value);
+                } else {
+                    maxpq.insert(value);
+                }
+                if (maxpq.size() - minpq.size() > 1) {
+                    float value1 = maxpq.delMax();
+                    minpq.insert(value1);
+                } else if (minpq.size() - maxpq.size() > 1) {
+                    float value1 = minpq.delMin();
+                    maxpq.insert(value1);
+                }
+                if (maxpq.size() == minpq.size()) {
+                    median = (minpq.min() + maxpq.max()) / 2;
+                }  else if (maxpq.size() > minpq.size()) {
+                    median = maxpq.max();
+                } else {
+                    median = minpq.min();
+                }
+                System.out.println(median);
+            }
+        }
+    }
 }
