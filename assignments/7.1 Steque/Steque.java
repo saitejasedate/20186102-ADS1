1357202c0013d08d59be828f35c0756b26e7ee68
saitejasedate <saitejavelagandula@msitprogram.net>
2018-10-26 17:38:03 +0530

    "ads1_m7_a1: 0/2 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/7.1 Steque/Steque.java
+++ assignments/7.1 Steque/Steque.java
@@ -43,7 +43,7 @@ public String toString() {
 	String str = "";
 	Node temp = start;
 	if (temp == null) {
-		return "Steque is Empty";
+		return "Steque is empty";
 	}
 	while (temp.next != null) {
         str += temp.data + "," + " ";
