5ccdce42ceca5d1dd476299b7d6b183297e2327d
saitejasedate <saitejavelagandula@msitprogram.net>
2018-10-26 17:57:34 +0530

    "ads1_m7_a1: 2/2 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- assignments/7.1 Steque/Steque.java
+++ assignments/7.1 Steque/Steque.java
@@ -5,35 +5,35 @@
  * Class for steque.
  */
 class Steque {
-	/**
-	 * Node start.
-	 */
-	private Node start = null;
-	/**
-	 * Node end.
-	 */
-	private Node end = null;
+    /**
+     * Node start.
+     */
+    private Node start = null;
+    /**
+     * Node end.
+     */
+    private Node end = null;
 /**
  * Class for node.
  */
 class Node {
-	/**
-	 * String data in a node.
-	 */
-	private String data;
-	/**
-	 * link for the next node.
-	 */
-	private Node next;
-	/**
-	 * Constructs the object.
-	 *
-	 * @param      data1  The data
-	 */
-	Node(final String data1)  {
-		this.data = data1;
-		this.next = null;
-	}
+    /**
+     * String data in a node.
+     */
+    private String data;
+    /**
+     * link for the next node.
+     */
+    private Node next;
+    /**
+     * Constructs the object.
+     *
+     * @param      data1  The data
+     */
+    Node(final String data1)  {
+        this.data = data1;
+        this.next = null;
+    }
 }
 /**
  * add item to the left of steque.
@@ -42,12 +42,12 @@ class Node {
  * @param      item  The item
  */
 public void push(final String item) {
-	Node newNode = new Node(item);
-	if (start == null) {
-		end = newNode;
-	}
-	newNode.next = start;
-	start = newNode;
+    Node newNode = new Node(item);
+    if (start == null) {
+        end = newNode;
+    }
+    newNode.next = start;
+    start = newNode;
 }
 /**
  * add item to the right of steque.
@@ -55,27 +55,27 @@ public void push(final String item) {
  * @param      item  The item
  */
 public void enqueue(final String item) {
-	if (start == null) {
-		Node newNode = new Node(item);
-		start = newNode;
-		end = newNode;
-	} else {
-		Node newNode1 = new Node(item);
-		end.next = newNode1;
-		end = newNode1;
-	}
+    if (start == null) {
+        Node newNode = new Node(item);
+        start = newNode;
+        end = newNode;
+    } else {
+        Node newNode1 = new Node(item);
+        end.next = newNode1;
+        end = newNode1;
+    }
 }
 /**
  * remove the top most element.
  * time complexity is constant.
  */
 public void pop() {
-	if (start != null) {
-		start = start.next;
-	}
-	if (end != null) {
-		end = end.next;
-	}
+    if (start != null) {
+        start = start.next;
+    }
+    if (end != null) {
+        end = end.next;
+    }
 }
 /**
  * Returns a string representation of the object.
@@ -83,12 +83,12 @@ public void pop() {
  * @return     String representation of the object.
  */
 public String toString() {
-	String str = "";
-	Node temp = start;
-	if (temp == null) {
-		return "Steque is empty.";
-	}
-	while (temp.next != null) {
+    String str = "";
+    Node temp = start;
+    if (temp == null) {
+        return "Steque is empty.";
+    }
+    while (temp.next != null) {
         str += temp.data + "," + " ";
         temp = temp.next;
     }
