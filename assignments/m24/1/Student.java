d6a02e12a1e23f92537e9f55e617f47573d18f69
saitejasedate <saitejavelagandula@msitprogram.net>
2018-10-27 12:59:25 +0530

    "m24_a1_1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m24/1/Student.java
+++ assignments/m24/1/Student.java
@@ -1,16 +1,44 @@
-import java.util.Scanner;
+/**
+ * Class for student.
+ */
 class Student {
+  /**
+   * rollnum.
+   */
   private int rollnum;
+  /**
+   * studname.
+   */
   private String name;
+  /**
+   * marks.
+   */
   private double marks;
+  /**
+   * Constructs the object.
+   *
+   * @param      rollnum   The rollnum
+   * @param      studname  The studname
+   * @param      marks     The marks
+   */
   Student(final int rollnum, final String studname, final double marks) {
     this.rollnum = rollnum;
     this.name = studname;
     this.marks = marks;
   }
+  /**
+   * Gets the name.
+   *
+   * @return     The name.
+   */
   public String getName() {
     return this.name;
   }
+  /**
+   * Gets the marks.
+   *
+   * @return     The marks.
+   */
   public double getMarks() {
     return this.marks;
   }
