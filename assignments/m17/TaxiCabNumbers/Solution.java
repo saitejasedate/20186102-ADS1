35538f1cb44fa086a90847b9bbcdf6e261d194e2
saitejasedate <saitejavelagandula@msitprogram.net>
2018-10-28 22:29:25 +0530

    "taxi_cab_numbers: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- assignments/m17/TaxiCabNumbers/Solution.java
+++ assignments/m17/TaxiCabNumbers/Solution.java
@@ -6,11 +6,19 @@ import java.util.Scanner;
  * Class for solution.
  */
 class Solution {
+	/**
+	 * Constructs the object.
+	 */
 	Solution () {
 		/**
 		 * Empty constructor.
 		 */
 	}
+	/**
+	 * main function to find the taxi cab number.
+	 *
+	 * @param      args  The arguments
+	 */
 	public static void main(String[] args) {
 		Scanner sc =new Scanner(System.in);
 		int N = sc.nextInt();
