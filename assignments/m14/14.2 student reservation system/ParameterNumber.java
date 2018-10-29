cfe4474348752725234cdb07d58bdc9143e0318b
saitejasedate <saitejavelagandula@msitprogram.net>
2018-10-29 13:52:25 +0530

    "ads1_m14_a2: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ assignments/m14/14.2 student reservation system/ParameterNumber.java
@@ -0,0 +1,7 @@
+public class ParameterNumber extends AbstractCheck {
+	<module name="ParameterNumber">
+    <property name="max" value="10"/>
+    <property name="ignoreOverriddenMethods" value="true"/>
+	</module>
+}
+ 
