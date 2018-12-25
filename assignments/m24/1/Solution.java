daffd957eeab7dd4e7acc34f636b664c0ab4fdb8
saitejasedate <saitejavelagandula@msitprogram.net>
2018-12-25 20:12:53 +0530

    "m24_a1_1: 2/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m24/1/Solution.java
+++ assignments/m24/1/Solution.java
@@ -9,27 +9,27 @@ class Solution {
 			Student obj = new Student(Integer.parseInt(str[0]), str[1], Double.parseDouble(str[2]));
 			st.put(Integer.parseInt(str[0]), obj);
 		}
-		// int queries = Integer.parseInt(sc.nextLine());
-		// for (int j = 0; j < queries; j++) {
-		// 	String[] str1 = sc.nextLine().split(",");
-		// 	switch (str1[2]) {
-		// 		case "1":
-		// 		try {
-		// 			System.out.println(st.get(Integer.parseInt(str1[1])).getName());
-		// 		} catch (Exception e) {
-		// 			System.out.println("Student doesn't exist");
-		// 		}
-		// 		break;
-		// 		case "2":
-		// 		try {
-		// 			System.out.println(st.get(Integer.parseInt(str1[1])).getMarks());
-		// 		} catch (Exception e) {
-		// 			System.out.println("Student doesn't exist");
-		// 		}
-		// 		break;
-		// 		default:
-		// 		break;
-		// 	} 
-		// }
+		int queries = Integer.parseInt(sc.nextLine());
+		for (int j = 0; j < queries; j++) {
+			String[] str1 = sc.nextLine().split(" ");
+			switch (str1[2]) {
+				case "1":
+				try {
+					System.out.println(st.get(Integer.parseInt(str1[1])).getName());
+				} catch (Exception e) {
+					System.out.println("Student doesn't exist");
+				}
+				break;
+				case "2":
+				try {
+					System.out.println(st.get(Integer.parseInt(str1[1])).getMarks());
+				} catch (Exception e) {
+					System.out.println("Student doesn't exist");
+				}
+				break;
+				default:
+				break;
+			} 
+		}
 	}
 }
