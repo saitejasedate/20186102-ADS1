06226458c500dec01854f0df98599ada5f570e6e
saitejasedate <saitejavelagandula@msitprogram.net>
2018-12-27 10:09:21 +0530

    "m24_a2_1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m24/2/Solution.java
+++ assignments/m24/2/Solution.java
@@ -3,11 +3,11 @@ class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int numOfdataRecords = Integer.parseInt(sc.nextLine());
-		BST<Integer, Student> bst = new BST<Integer, Student>();
+		BST<Double, Student> bst = new BST<Double, Student>();
 		for (int i = 0; i < numOfdataRecords; i++) {
 			String[] arr = sc.nextLine().split(",");
 			Student obj = new Student(Integer.parseInt(arr[0]), arr[1], Double.parseDouble(arr[2]));
-			bst.put(Integer.parseInt(arr[2]), obj);
+			bst.put(Double.parseDouble(arr[2]), obj);
 		}
 		int numOfqueries = Integer.parseInt(sc.nextLine());
 		for (int j = 0; j < numOfqueries; j++) {
@@ -15,7 +15,7 @@ class Solution {
 			switch (arr1[0]) {
 				case "BE":
 				for (int k = Integer.parseInt(arr1[1]); k <= Integer.parseInt(arr1[2]); k++) {
-					System.out.println(bst.get(Integer.parseInt(arr1[k])).getstudentname());
+					System.out.println(bst.get(Double.parseDouble(arr1[k])));
 				}
 				case "GE":
 				case "LE":
