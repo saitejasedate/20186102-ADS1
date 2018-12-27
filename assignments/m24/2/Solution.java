23d373faeb07975058d06a905f756e3ea501c703
saitejasedate <saitejavelagandula@msitprogram.net>
2018-12-27 12:33:48 +0530

    "m24_a2_1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m24/2/Solution.java
+++ assignments/m24/2/Solution.java
@@ -12,10 +12,18 @@ class Solution {
 		int numOfqueries = Integer.parseInt(sc.nextLine());
 		for (int j = 0; j < numOfqueries; j++) {
 			String[] arr1 = sc.nextLine().split(" ");
+			int n = 0;
 			switch (arr1[0]) {
 				case "BE":
-				for (int k = Integer.parseInt(arr1[1]); k <= Integer.parseInt(arr1[2]); k++) {
-					System.out.println(bst.get(Double.parseDouble(arr1[k])));
+				System.out.println(bst.size());
+				try {
+					for (double k = 0; k < bst.size(); k++) {
+						if ((bst.get(k)).getmarks() >= Double.parseDouble(arr1[1]))  {
+							System.out.println((bst.get(k)).getstudentname());
+						}
+					}
+				} 	catch (Exception e) {
+					System.out.println("o");
 				}
 				case "GE":
 				case "LE":
