7ea388ea56997fdf787e463bc2b3d914f7f9e722
saitejasedate <saitejavelagandula@msitprogram.net>
2018-12-29 10:33:05 +0530

    "ads1_assessment_assignment1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/Team Ranking/Team Ranking (copy)/Solution.java
+++ assignments/Team Ranking/Team Ranking (copy)/Solution.java
@@ -6,21 +6,16 @@ class Solution {
 		while (sc.hasNext()) {
 			sb.append(sc.next() + "::");
 		}
-		System.out.println(sb);
 		String[] lines = sb.toString().split("::");
-		for (int j = 0; j < lines.length; j++) {
-			System.out.println(lines[j]);
-		}
 		int i = 0;
 		Team[] teams = new Team[lines.length];
 		for (String line : lines) {
-			String[] tokens = line.split("");
+			String[] tokens = line.split(" ");
 			Team team = new Team(tokens[0], Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2]),
 				Integer.parseInt(tokens[3]));
 			teams[i++] = team;
 		}
 		Insertion.sort(teams);
-		String output = "";
 		for (Team team : teams) {
 			System.out.println(team);
 		}
