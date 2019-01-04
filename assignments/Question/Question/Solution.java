6c2827294586c69ad444ffc05645d82a7916f7f3
saitejasedate <saitejavelagandula@msitprogram.net>
2019-01-04 12:38:02 +0530

    "remedial_ADS1_M1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/Question/Question/Solution.java
+++ assignments/Question/Question/Solution.java
@@ -1,37 +1,24 @@
-import java.util.HashMap;
+import java.util.Stack;
 import java.util.Map;
 import java.util.Scanner;
 class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		HashMap<Integer, Integer> map = new HashMap<>();
+		Stack<Integer> s1 = new Stack<Integer>();
 		StringBuffer sb = new StringBuffer();
 		while (sc.hasNext()) {
 			sb.append(sc.nextLine() + "::");
-			// System.out.println(sb);
 		}
 		String[] lines = sb.toString().split("::");
 		for (String line : lines) {
-			if (line.equals("JC")) {
-				print(map);
+			if (line != "JC") {
+				// String[] tokens = line.split(",");
+				s1.push(Integer.parseInt(line));
+				System.out.println(s1); 
 			}
 			else {
 				String[] tokens = line.split(",");
-				map.put(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]));
 			}
 		}
-	}
-
-	public static void print(Map<Integer, Integer> map)  
-    { 
-        if (map.isEmpty())  
-        { 
-            System.out.println("NO Registration"); 
-        } 
-          
-        else
-        { 
-            System.out.println(map); 
-        } 
-    } 
+	} 
 }
