fa52aa5cadc6b7b49f947793446f923586a717eb
saitejasedate <saitejavelagandula@msitprogram.net>
2019-01-04 11:56:45 +0530

    "remedial_ADS1_M1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/Question/Question/Solution.java
+++ assignments/Question/Question/Solution.java
@@ -1,39 +1,17 @@
-// import java.util.Scanner;
-// class Solution {
-// 	public static void main(String[] args) {
-// 		Scanner sc = new Scanner(System.in);
-// 		SeparateChainingHashST<Integer, Integer> st = new SeparateChainingHashST<Integer, Integer>();
-// 		// while (sc.hasNextLine()) {
-// 			if (sc.nextLine() == "JC") {
-// 				if (st.isEmpty()) {
-// 					System.out.println("No registration");
-// 				}
-// 				else {
-// 					st.keys();
-// 				}
-// 			}
-// 			else {
-// 				String[] str = sc.nextLine().split(",");
-// 				st.put(Integer.parseInt(str[0]), Integer.parseInt(str[1]));
-// 				System.out.println(st.keys());
-// 		}
-// 	}
-// }
-// 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		HashMap<Integer, Person> map = new HashMap<>();
+		HashMap<Integer, Integer> map = new HashMap<>();
 		StringBuffer sb = new StringBuffer();
 		while (sc.hasNext()) {
 			sb.append(sc.nextLine() + "::");
 		}
 		String[] lines = sb.toString().split("::");
-		System.out.println(lines[0]);
-		Person[] persons = new Person[lines.length];
+		// System.out.println(lines[0]);
+		// Person[] persons = new Person[lines.length];
 		for (String line : lines) {
 			if (line.equals("JC")) {
 				if (map.isEmpty()) {
@@ -46,14 +24,13 @@ class Solution {
 			}
 			else {
 				String[] tokens = line.split(",");
-				Person person = new Person(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]));
-				map.put(Integer.parseInt(tokens[0]), person);
+				map.put(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]));
 				print(map);
 			}
 		}
 	}
 
-	public static void print(Map<Integer, Person> map)  
+	public static void print(Map<Integer, Integer> map)  
     { 
         if (map.isEmpty())  
         { 
