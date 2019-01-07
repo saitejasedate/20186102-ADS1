d08ceb019a7023f923c13a19b864927a8f9ddd57
saitejasedate <saitejavelagandula@msitprogram.net>
2019-01-07 16:53:32 +0530

    "remedial_ADS1_M1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/Question/Question/Solution.java
+++ assignments/Question/Question/Solution.java
@@ -1,24 +1,11 @@
-import java.util.Stack;
-import java.util.Map;
 import java.util.Scanner;
 class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		Stack<Integer> s1 = new Stack<Integer>();
 		StringBuffer sb = new StringBuffer();
 		while (sc.hasNext()) {
-			sb.append(sc.nextLine() + "::");
+			sb.append(sc.nextLine()+"::");
 		}
-		String[] lines = sb.toString().split("::");
-		for (String line : lines) {
-			if (line != "JC") {
-				// String[] tokens = line.split(",");
-				s1.push(Integer.parseInt(line));
-				System.out.println(s1.toString()); 
-			}
-			else {
-				String[] tokens = line.split(",");
-			}
-		}
-	} 
+		System.out.println(sb);
+	}
 }
