1ca84a2fec6100be5741bee4586d4e5a1eba9551
saitejasedate <saitejavelagandula@msitprogram.net>
2019-01-08 12:02:25 +0530

    "remedial_ADS1_M1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ assignments/Question/Question/Jury.java
@@ -0,0 +1,23 @@
+class Jury {
+	private int regst;
+	private int sectn;
+	Jury (int regst, int sectn) {
+		this.regst = regst;
+		this.sectn = sectn;
+	}
+	public int getregst(){
+		return regst;
+	}
+
+	public void setregst(int regst){
+		this.regst = regst;
+	}
+
+	public int getsectn(){
+		return sectn;
+	}
+
+	public void setsectn(int sectn){
+		this.sectn = sectn;
+	}
+}
--- assignments/Question/Question/Solution.java
+++ assignments/Question/Question/Solution.java
@@ -1,13 +1,25 @@
 import java.util.Scanner;
 class Solution {
 	public static void main(String[] args) {
-		LinearProbingHashST<Integer, Integer> lst = new LinearProbingHashST<Integer, Integer>(20);
+		SeparateChainingHashST<Integer, Jury> lst = new SeparateChainingHashST<Integer, Jury>(20);
 		Scanner sc = new Scanner(System.in);
 		StringBuffer sb = new StringBuffer();
 		while (sc.hasNext()) {
 			sb.append(sc.nextLine()+"::");
 		}
 		String[] str = sb.toString().split("::");
-		System.out.println(lst.size());
+		for (int i = 0; i < str.length; i++) {
+			if (str[i].equals("JC")) {
+				System.out.println("NO Registration");
+			}
+			else {
+				String[] str1 = str[i].split(",");
+				Jury jury = new Jury(Integer.parseInt(str1[0]), Integer.parseInt(str1[1]));
+				// switch(str1[1]) {
+				// 	case "1":
+				// }
+				lst.put(Integer.parseInt(str1[1]), jury);
+			}
+		}
 	}
 }
