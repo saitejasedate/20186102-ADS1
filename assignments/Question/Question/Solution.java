8b141c1ca666517094a804e600280852bfc4d086
saitejasedate <saitejavelagandula@msitprogram.net>
2019-01-08 14:26:22 +0530

    "remedial_ADS1_M1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/Question/Question/Solution.java
+++ assignments/Question/Question/Solution.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 class Solution {
+	public static int c;
+	public static String[] str1;
 	public static void main(String[] args) {
+		int c = 0;
 		SeparateChainingHashST<Integer, Jury> lst = new SeparateChainingHashST<Integer, Jury>();
 		Scanner sc = new Scanner(System.in);
 		StringBuffer sb = new StringBuffer();
@@ -10,22 +13,22 @@ class Solution {
 		String[] str = sb.toString().split("::");
 		for (int i = 0; i < str.length; i++) {
 			if (str[i].equals("JC")) {
-				// print();
-				System.out.println("N");
+				print();
+				// System.out.println("N");
 			}
 			else {
 				String[] str1 = str[i].split(",");
 				Jury jury = new Jury(Integer.parseInt(str1[0]), Integer.parseInt(str1[1]));
-				// switch(str1[1]) {
-				// 	case "1":
-				// }
 				lst.put(Integer.parseInt(str1[1]), jury);
-				System.out.println(lst);
+				c++;
 			}
 		}
 	}
-	// public static lst print() {
-	// 	SeparateChainingHashST<Integer, Jury> lst = new SeparateChainingHashST<Integer, Jury>();
-	// 	return lst;
-	// } 
+	public static Jury print() {
+		SeparateChainingHashST<Integer, Jury> lst = new SeparateChainingHashST<Integer, Jury>();
+		if (c == 1) {
+			return lst.get(Integer.parseInt(str1[1]));
+		}
+		return null;
+	} 
 }
