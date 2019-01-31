5f5d85123c4b5a6767d8053ceb7edf3a7208cb95
saitejasedate <saitejavelagandula@msitprogram.net>
2019-01-31 14:16:34 +0530

    "m24_a2_1: 1/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m24/2/Solution.java
+++ assignments/m24/2/Solution.java
@@ -3,31 +3,49 @@ class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		BST<Double, Student> bst = new BST<Double, Student>();
+		LinearProbingHashST<Double, Student> lst = new LinearProbingHashST<Double, Student>();
 		int num = Integer.parseInt(sc.nextLine());
 		int count = 0;
 		while (count < num) {
 			String[] str = sc.nextLine().split(",");
 			Student std = new Student(Integer.parseInt(str[0]), str[1], Double.parseDouble(str[2]));
 			bst.put(Double.parseDouble(str[2]), std);
+			lst.put(Double.parseDouble(str[2]), std);
 			count++;
 		}
+		// System.out.println("--------------------------------------------------");
+		// for (Double s : bst.keys()) {
+		// 	System.out.println(s + " " + bst.get(s));
+  //           // StdOut.println(s + " " + bst.get(s));
+		// }
+		// System.out.println(bst.size());
+		// System.out.println("---------------------------------------------------");
+		// for (Double s : bst.keys()) {
+		// 	System.out.println(s + " " + lst.get(s));
+		// }
 		int queries = Integer.parseInt(sc.nextLine());
 		for (int i = 0; i < queries; i++) {
 			String[] arr = sc.nextLine().split(" ");
-			switch("arr[0]") {
+			switch(arr[0]) { 
 				case"BE":
-				for (Double j = Double.parseDouble(arr[1]); j <= Double.parseDouble(arr[2]); j++) {
-					System.out.println(bst.get(j));
-				}
+				for (Double s : bst.keys()) {
+					if (s >= Double.parseDouble(arr[1]) && s <= Double.parseDouble(arr[2])) {
+						System.out.println(bst.get(s).getname());
+					}
+				}			
 				break;
 				case"LE":
-				for (Double j = Double.parseDouble(arr[1]); j >= 0; j--) {
-					System.out.println(bst.get(j));
+				for (Double s : bst.keys()) {
+					if (s <= Double.parseDouble(arr[1])) {
+						System.out.println(bst.get(s));
+					}
 				}
 				break;
 				case"GE":
-				for (Double j = Double.parseDouble(arr[1]); j <= bst.size(); j++) {
-					System.out.println(bst.get(j));
+				for (Double s : bst.keys()) {
+					if (s >= Double.parseDouble(arr[1])) {
+						System.out.println(bst.get(s));
+					}
 				}
 				break;
 				default:
