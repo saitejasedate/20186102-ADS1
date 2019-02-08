1c1a03ba78606fbb2b43f261c2cd66abbc993811
saitejasedate <saitejavelagandula@msitprogram.net>
2019-02-08 10:29:37 +0530

    "ads1_remedial_3_a1_1: 0/7 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/ADS-1 Remedial-3/Remedial-3/Solution.java
+++ assignments/ADS-1 Remedial-3/Remedial-3/Solution.java
@@ -3,23 +3,39 @@ class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int numOfrecords = Integer.parseInt(sc.nextLine());
-		BST<Double, Student> bst = new BST<Double, Student>();
+		BST<Integer, Student> bst = new BST<Integer, Student>();
+		LinearProbingHashST<Integer, Student> lst = new LinearProbingHashST<Integer, Student>();
 		int count = 0;
 		while (count < numOfrecords) {
 			String[] str = sc.nextLine().split(",");
 			Student st = new Student(Integer.parseInt(str[0]), str[1], Double.parseDouble(str[2]));
-			bst.put(Double.parseDouble(str[2]), st);
+			if (bst.contains(Integer.parseInt(str[0]))) {
+				lst.put(Integer.parseInt(str[0]), st);
+			} else {
+				bst.put(Integer.parseInt(str[0]), st);
+			}
 			count++;
 		}
-		// for (Double s : bst.keys()) {
-		// 	System.out.println(bst.get(s));
-		// }
+		System.out.println("-----------------------------");
+		for (int s : bst.keys()) {
+			System.out.println(s + ", "+ bst.get(s));
+		}
+		System.out.println("--------------------------------");
+		for (int s : bst.keys()) {
+			System.out.println(s + ", "+ lst.get(s));
+		}
+		System.out.println("-------------------------------");
 		int numOfqueries = Integer.parseInt(sc.nextLine());
 		for (int i = 0; i < numOfqueries; i++) {
-			int query = Integer.parseInt(sc.nextLine());
-			for (Double s : bst.keys()) {
-				if (query == s) {
+			int query = sc.nextInt();
+			for (int s : bst.keys()) {
+				if (bst.get(s).getmarks() == query) {
 					System.out.println(bst.get(s));
+				}
+				if (lst.contains(s)) {
+					if (bst.get(s).getmarks() == query) {
+						System.out.println(lst.get(s));
+					}
 				}
 			}
 		}
