5d84e3dd6b7ab70425769421967cc226229b847b
saitejasedate <saitejavelagandula@msitprogram.net>
2019-02-08 10:52:21 +0530

    "ads1_remedial_3_a1_1: 1/7 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/ADS-1 Remedial-3/Remedial-3/Solution.java
+++ assignments/ADS-1 Remedial-3/Remedial-3/Solution.java
@@ -3,16 +3,16 @@ class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int numOfrecords = Integer.parseInt(sc.nextLine());
-		BST<Integer, Student> bst = new BST<Integer, Student>();
-		LinearProbingHashST<Integer, Student> lst = new LinearProbingHashST<Integer, Student>();
+		BST<String, Student> bst = new BST<String, Student>();
+		LinearProbingHashST<String, Student> lst = new LinearProbingHashST<String, Student>();
 		int count = 0;
 		while (count < numOfrecords) {
 			String[] str = sc.nextLine().split(",");
 			Student st = new Student(Integer.parseInt(str[0]), str[1], Double.parseDouble(str[2]));
-			if (bst.contains(Integer.parseInt(str[0]))) {
-				lst.put(Integer.parseInt(str[0]), st);
+			if (bst.contains((str[1]))) {
+				lst.put((str[1]), st);
 			} else {
-				bst.put(Integer.parseInt(str[0]), st);
+				bst.put((str[1]), st);
 			}
 			count++;
 		}
@@ -27,13 +27,13 @@ class Solution {
 		// System.out.println("-------------------------------");
 		int numOfqueries = Integer.parseInt(sc.nextLine());
 		for (int i = 0; i < numOfqueries; i++) {
-			int query = Integer.parseInt(sc.nextLine());
-			for (int s : bst.keys()) {
-				if (bst.get(s).getmarks() == query) {
+			String query = sc.nextLine();
+			for (String s : bst.keys()) {
+				if (bst.get(s).getmarks() == Integer.parseInt(query)) {
 					System.out.println(bst.get(s));
 				}
 				if (lst.contains(s)) {
-					if (bst.get(s).getmarks() == query) {
+					if (bst.get(s).getmarks() == Integer.parseInt(query)) {
 						System.out.println(lst.get(s));
 					}
 				}
